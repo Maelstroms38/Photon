dd5421b14fb83a032b93a55426d648d5f2880a57
Maelstroms38 <stroms38@gmail.com>
2014-10-26 11:14:28 -0400

    Initial Commit

---
+++ Photon/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Photon
+//
+//  Created by Michael Stromer on 10/26/14.
+//  Copyright (c) 2014 Michael Stromer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
